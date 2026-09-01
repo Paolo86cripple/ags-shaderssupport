bfb83fc6deb6e19612c7921063b11a49669f47f8
Paolo86cripple <pbossi86@gmail.com>
2026-09-01 13:15:31 +0000

    Shaders: add CRT GLSL test shader

---
+++ Engine/gfx/shaders/crt-simple.glsl
@@ -0,0 +1,14 @@
+#version 120
+
+uniform sampler2D uTexture;
+uniform vec2 uInputSize;
+varying vec2 vTexCoord;
+
+void main()
+{
+    vec3 color = texture2D(uTexture, vTexCoord).rgb;
+    float scanline = 0.94 + 0.06 * cos(vTexCoord.y * uInputSize.y * 3.14159265);
+    float d = length(vTexCoord - vec2(0.5));
+    float vignette = 1.0 - 0.12 * d * d;
+    gl_FragColor = vec4(color * scanline * vignette, 1.0);
+}
