92ffbf6bb7692edbefe3c6c7ec5593d82bc9d6f3
Paolo86cripple <pbossi86@gmail.com>
2026-09-01 13:29:33 +0000

    Shader injector: make per-game test launcher usable

--- injector/run-with-shader.sh
+++ injector/run-with-shader.sh
@@ -2,12 +2,13 @@
 set -eu
 
 HERE=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
-GAME=${1:?usage: $0 /path/to/game.exe [game args...]}
+GAME=${1:?usage: $0 /path/to/game [game args...]}
 shift || true
 
-SHADER=${AGS_SHADER_CHAIN:-"$HERE/shaders/crt.agschain"}
+SHADER=${AGS_SHADER_CHAIN:-"$HERE/shaders/invert.glsl"}
 
 export AGS_SHADER_CHAIN="$SHADER"
+export AGS_SHADER_DEBUG="${AGS_SHADER_DEBUG:-1}"
 export LD_PRELOAD="$HERE/libags-shader.so${LD_PRELOAD:+:$LD_PRELOAD}"
 
 exec "$GAME" "$@"
