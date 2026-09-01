5cdc172d778c16417234133d78cd043c9997da3b
Paolo86cripple <pbossi86@gmail.com>
2026-09-01 13:15:43 +0000

    Shaders: add multipass blur test shader

---
+++ Engine/gfx/shaders/blur-pass.glsl
@@ -0,0 +1,15 @@
+#version 120
+
+uniform sampler2D uTexture;
+uniform vec2 uTexelSize;
+varying vec2 vTexCoord;
+
+void main()
+{
+    vec4 c = texture2D(uTexture, vTexCoord) * 0.36;
+    c += texture2D(uTexture, vTexCoord + vec2(uTexelSize.x, 0.0)) * 0.16;
+    c += texture2D(uTexture, vTexCoord - vec2(uTexelSize.x, 0.0)) * 0.16;
+    c += texture2D(uTexture, vTexCoord + vec2(0.0, uTexelSize.y)) * 0.16;
+    c += texture2D(uTexture, vTexCoord - vec2(0.0, uTexelSize.y)) * 0.16;
+    gl_FragColor = c;
+}
