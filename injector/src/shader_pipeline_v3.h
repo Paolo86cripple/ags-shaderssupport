b48dc6b08d7f68ea4bad751a025facb26ce2ab51
Paolo86cripple <pbossi86@gmail.com>
2026-09-03 20:59:59 +0000

    Add per-pass texture history state

--- injector/src/shader_pipeline_v3.h
+++ injector/src/shader_pipeline_v3.h
@@ -10,9 +10,24 @@ public:
     void clear();
     void apply(unsigned input_texture, int input_width, int input_height, int output_width, int output_height);
 private:
-    struct Pass { unsigned program=0; int texture=-1,input_size=-1,texture_size=-1,output_size=-1,original_size=-1,texel_size=-1,frame_count=-1,frame_direction=-1,time=-1; bool filter_linear=true; ScaleType scale_type_x=ScaleType::Source,scale_type_y=ScaleType::Source; float scale_x=1.f,scale_y=1.f; std::string source_path; };
+    static constexpr int MaxPrevPasses = 8;
+    struct Pass {
+        unsigned program=0;
+        int texture=-1,input_size=-1,texture_size=-1,output_size=-1,original_size=-1,texel_size=-1,frame_count=-1,frame_direction=-1,time=-1,mvp_matrix=-1;
+        int prev_texture[MaxPrevPasses];
+        int prev_texture_size[MaxPrevPasses];
+        bool filter_linear=true;
+        ScaleType scale_type_x=ScaleType::Source,scale_type_y=ScaleType::Source;
+        float scale_x=1.f,scale_y=1.f;
+        std::string source_path;
+        Pass() {
+            for (int i=0;i<MaxPrevPasses;++i) { prev_texture[i]=-1; prev_texture_size[i]=-1; }
+        }
+    };
     struct Target { unsigned fbo=0,texture=0; int width=0,height=0; };
-    std::vector<Pass> _passes; Target _targets[2]; unsigned long long _frame_count=0;
+    std::vector<Pass> _passes;
+    std::vector<Target> _targets;
+    unsigned long long _frame_count=0;
     bool load_text(const std::string&,std::string&,std::string&) const;
     bool add_pass(const std::string&,const Pass*,std::string&);
     bool create_program(const std::string&,const std::string&,unsigned&,std::string&) const;
