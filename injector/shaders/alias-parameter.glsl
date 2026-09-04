39903d4966ed1f18cbaad0712304631a2773ad28
Paolo86cripple <pbossi86@gmail.com>
2026-09-04 12:54:12 +0000

    Use canonical alias texture naming in regression fixture

--- injector/shaders/alias-parameter.glsl
+++ injector/shaders/alias-parameter.glsl
@@ -1,11 +1,15 @@
 #version 120
+
 #pragma parameter Gain "Gain" 0.5 0.0 1.0 0.1
-uniform sampler2D BASE;
-uniform vec2 BASESize;
+
+uniform sampler2D BASETexture;
+uniform vec2 BASETextureSize;
 uniform float Gain;
 varying vec2 vTexCoord;
+
 void main()
 {
-    vec4 c = texture2D(BASE, vTexCoord);
-    gl_FragColor = vec4(c.rgb * Gain, c.a);
+    vec4 c = texture2D(BASETexture, vTexCoord);
+    float valid = step(0.5, BASETextureSize.x) * step(0.5, BASETextureSize.y);
+    gl_FragColor = vec4(c.rgb * Gain * valid, c.a);
 }
