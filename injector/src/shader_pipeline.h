c3d5072ad0c2b80aebf98c65cbe99dc34e041e97
Paolo86cripple <pbossi86@gmail.com>
2026-09-04 21:18:29 +0000

    injector: add GPU source-resample state

--- injector/src/shader_pipeline.h
+++ injector/src/shader_pipeline.h
@@ -6,21 +6,31 @@
 
 class ShaderPipeline {
 public:
+    ~ShaderPipeline();
+
     bool load(const std::string &path, std::string &error);
     bool loaded() const { return _impl.loaded(); }
-    void clear() { _impl.clear(); }
+    void clear();
     void apply(unsigned input_texture,
                int input_width,
                int input_height,
                int output_width,
-               int output_height) {
-        _impl.apply(input_texture,
-                    input_width,
-                    input_height,
-                    output_width,
-                    output_height);
-    }
+               int output_height);
 
 private:
     ShaderPipelineV4 _impl;
+    unsigned _source_texture = 0;
+    unsigned _source_read_fbo = 0;
+    unsigned _source_draw_fbo = 0;
+    int _source_width = 0;
+    int _source_height = 0;
+    bool _gpu_info_logged = false;
+
+    void release_source_resampler();
+    bool prepare_source_texture(unsigned input_texture,
+                                int input_width,
+                                int input_height,
+                                unsigned &source_texture,
+                                int &source_width,
+                                int &source_height);
 };
